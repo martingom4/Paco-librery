0fe16f89806f3033ff44e1f806e93f6e82ca4ebe
martingom4 <martingomez9636@gmail.com>
2024-11-13 17:07:57 -0500

    controllers defined for all models

---
+++ src/app/routes.php
@@ -0,0 +1,3 @@
+<?php
+
+#en este archivos se van a poner las rutas que vamos a usar en el sistemas, es decir como queremos que el usuarios navegue por nuestra pagina web
--- src/app/views/cliente/registrocliente1.php
+++ src/app/views/cliente/registrocliente1.php
@@ -1,5 +1,4 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<%@ page import="java.sql.*" %>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -7,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro de Cliente</title>
     <link rel="shortcut icon" href="../images/LOGO.png" alt="logo">
-    <link rel="stylesheet" href="../css/registrocliente.css">
+    <link rel="stylesheet" href="../../../public/css/registrocliente.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
@@ -48,64 +47,13 @@
             <div class="registro-box">
                 <h2>Registro de Cliente</h2>
                 <hr>
-                <% 
-                    boolean registroExitoso = false;
-                    if (request.getMethod().equalsIgnoreCase("POST")) {
-                        // Obtener los parámetros del formulario
-                        String nombre = request.getParameter("nombre");
-                        String segundo_nombre = request.getParameter("segnombre");
-                        String apellido = request.getParameter("apellido");
-                        String segundo_apellido = request.getParameter("segapellido");
-                        String contrasena = request.getParameter("contrasena");
-                        String email = request.getParameter("email");
 
-                        if (nombre != null && apellido != null && contrasena != null && email != null) {
-                            Connection conexion = null;
-                            CallableStatement llamada = null;
-
-                            try {
-                                // Cargar el driver de Oracle
-                                Class.forName("oracle.jdbc.driver.OracleDriver");
-
-                                // Establecer la conexión con la base de datos Oracle
-                                String jdbcUrl = "jdbc:oracle:thin:@localhost:1521:xe";
-                                String username = "martin1"; // se ingresa el nombre del usuario de tu base de datos oracle 
-                                String password = "1234"; // se ingresa la contraseña de tu usuario 
-                                conexion = DriverManager.getConnection(jdbcUrl, username, password);
-
-                                // Llamar al procedimiento almacenado
-                                String procedimiento = "{call registrar_cliente(?, ?, ?, ?, ?, ?)}";
-                                llamada = conexion.prepareCall(procedimiento);
-
-                                // Establecer los parámetros del procedimiento
-                                llamada.setString(1, email);
-                                llamada.setString(2, nombre);
-                                llamada.setString(3, segundo_nombre);
-                                llamada.setString(4, apellido);
-                                llamada.setString(5, segundo_apellido);
-                                llamada.setString(6, contrasena);
-
-                                // Ejecutar el procedimiento almacenado
-                                llamada.execute();
-                                
-                                // Marcar el registro como exitoso
-                                registroExitoso = true;
-                            } catch (Exception e) {
-                                out.println("<h2>Error en el registro: " + e.getMessage() + "</h2>");
-                            } finally {
-                                if (llamada != null) try { llamada.close(); } catch (SQLException e) { e.printStackTrace(); }
-                                if (conexion != null) try { conexion.close(); } catch (SQLException e) { e.printStackTrace(); }
-                            }
-                        }
-                    }
-                    if (registroExitoso) {
-                %>
                 <div class="mensaje-exito">
                     <h2>REGISTRO EXITOSO</h2>
                     <p>Su registro se ha completado exitosamente.</p>
                     <a href="home.html">Volver a la página principal</a>
                 </div>
-                <% } else { %>
+
                 <form method="post">
                     <div class="input-group">
                         <input type="text" placeholder="Nombre" name="nombre" required>
@@ -119,7 +67,7 @@
                     <input type="email" placeholder="Email" name="email" required>
                     <button type="submit" class="btn">Confirmar</button>
                 </form>
-                <% } %>
+
             </div>
         </div>
     </main>
