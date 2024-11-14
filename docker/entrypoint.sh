6a3199bb7566abccdfce64dc6773f702a9ecf6bf
martingom4 <martingomez9636@gmail.com>
2024-11-13 20:25:02 -0500

    cambios en el docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,14 +5,14 @@ until pg_isready -h $DB_HOST -p $DB_PORT -U $DB_USERNAME; do
 done
 
 # Ejecutar migraciones solo si no se han ejecutado antes
-if [ ! -f /var/www/html/.migraciones_completadas ]; then
+if [ ! -f /var/www/.migraciones_completadas ]; then
   echo "Ejecutando migraciones..."
   if [ -f /var/www/html/migrations/init.sql ]; then
     export PGPASSWORD=$DB_PASSWORD
     psql -h $DB_HOST -U $DB_USERNAME -d $DB_DATABASE -f /var/www/html/migrations/init.sql
   fi
   # Crear un archivo de marca para indicar que las migraciones se ejecutaron
-  touch /var/www/html/.migraciones_completadas
+  touch /var/www/.migraciones_completadas
 fi
 
 echo "Configurando ServerName en Apache..."
