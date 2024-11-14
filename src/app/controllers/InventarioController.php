58e6c48f4560051c28d14e7365491f95553a3ea3
martingom4 <martingomez9636@gmail.com>
2024-11-13 20:37:00 -0500

    Configuracion de Fasroutes

---
+++ src/app/controllers/InventarioController.php
@@ -0,0 +1,11 @@
+<?php
+
+class InventarioController {
+    public function index() {
+        // Código para mostrar el inventario
+    }
+
+    public function agregarInventario() {
+        // Código para agregar al inventario
+    }
+}
