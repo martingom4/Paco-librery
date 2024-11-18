a2ea192a3e5f33824d32b3a561e230045aee7933
martingom4 <martingomez9636@gmail.com>
2024-11-18 12:42:45 -0500

    Cambio de base de datos a mysql

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-echo "Esperando a que PostgreSQL esté listo..."
-until pg_isready -h $DB_HOST -p $DB_PORT -U $DB_USERNAME; do
+set -e  # Detener el script si ocurre un error
+
+echo "Esperando a que MySQL esté listo..."
+until mysqladmin ping -h "$DB_HOST" --silent; do
+  echo "MySQL no está listo. Reintentando en 3 segundos..."
   sleep 3
 done
 
 # Ejecutar migraciones solo si no se han ejecutado antes
 if [ ! -f /var/www/.migraciones_completadas ]; then
   echo "Ejecutando migraciones..."
-  if [ -f /var/www/html/migrations/init.sql ]; then
-    export PGPASSWORD=$DB_PASSWORD
-    psql -h $DB_HOST -U $DB_USERNAME -d $DB_DATABASE -f /var/www/html/migrations/init.sql
+  if [ -f /var/www/migrations/init.sql ]; then
+    mysql -h "$DB_HOST" -u "$DB_USERNAME" -p"$DB_PASSWORD" "$DB_DATABASE" < /var/www/migrations/init.sql
   fi
   # Crear un archivo de marca para indicar que las migraciones se ejecutaron
   touch /var/www/.migraciones_completadas
@@ -17,5 +19,6 @@ fi
 
 echo "Configurando ServerName en Apache..."
 echo "ServerName localhost" >> /etc/apache2/apache2.conf
+
 echo "Iniciando Apache..."
 apache2-foreground
